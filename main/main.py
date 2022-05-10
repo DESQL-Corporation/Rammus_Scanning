0752bce8e67964b05979a6e2a4dad7dab71096b9
BERNAT Loan <loan.bernat@univ-tlse3.fr>
2022-05-10 15:15:36 +0200

    barre de config personalisable

--- main/main.py
+++ main/main.py
@@ -38,59 +38,38 @@ class Application(tk.Tk):
         self.iconbitmap('../img/logo.ico')
         self.grid()
 
+        self.affichageVertical = False
+        self.modeAutomatique = True
+
         '''     CREATION DES 2 BLOCS PRINCIPAUX    '''
         self.canvas = Canvas(self, background='ivory')
         self.barreControle = Frame(self, background='grey')
 
         '''     INITIALISATION DE LA CASE ETAT DE LA BARRE DE CONTROLE      '''
         self.caseEtat = Frame(self.barreControle, background="black")
+        self.telecomande = Frame(self.barreControle, background="grey")
 
         '''     INITIALISATION DES BOUTONS DE LA BARRE DE CONTROLE     '''
-        self.boutonStart = Button(self.barreControle, text="Start Scanning", fg="green", command=self.startModeAutomatique)
-        self.boutonPause = Button(self.barreControle, text="Pause Scanning", fg="orange", command=self.pauseModeAutomatique, state=DISABLED)
-        self.boutonStop = Button(self.barreControle, text="Stop Scanning", fg="red", command=self.stopModeAutomatique, state=DISABLED)
-        self.boutonManuel = Button(self.barreControle, text="Mode Manuel", command=self.clilcModeManuel, background="red")
-        self.boutonAvancer = Button(self.barreControle, text="Avancer", command=self.alert, state=DISABLED)
-        self.boutonDroite = Button(self.barreControle, text="Tourner à droite", command=self.alert, state=DISABLED)
-        self.boutonGauche = Button(self.barreControle, text="Tourner à gauche", command=self.alert, state=DISABLED)
+        self.boutonStart = Button(self.telecomande, text="Démarrer", fg="green", command=self.startModeAutomatique)
+        self.boutonPause = Button(self.telecomande, text="Mettre en Pause", fg="orange", command=self.pauseModeAutomatique, state=DISABLED)
+        self.boutonStop = Button(self.telecomande, text="Stopper", fg="red", command=self.stopModeAutomatique, state=DISABLED)
+        self.boutonAvancer = Button(self.telecomande, text="Avancer", command=self.alert, state=DISABLED)
+        self.boutonDroite = Button(self.telecomande, text="Tourner à droite", command=self.alert, state=DISABLED)
+        self.boutonGauche = Button(self.telecomande, text="Tourner à gauche", command=self.alert, state=DISABLED)
 
         '''     INITIALISATION DES ZONES DE TEXTES DE LA BARRE DE CONTROLE    '''
-        self.labelTempsRun = Label(self.barreControle, text="Temps : 00.00.00", background="gray")
-        self.labelCommandeAutomatique = Label(self.barreControle, text="Commande Automatique", fg="black", bg="gray")
-        self.labelCommandeManuelle = Label(self.barreControle, text="Commande Manuelle", fg="black", bg="gray")
+        self.labelTempsRun = Label(self.telecomande, text="Temps : 00.00.00", background="gray")
+        self.labelCommande = Label(self.telecomande, text="Commande Automatique", fg="black", bg="gray")
 
         '''     PLACEMENT DES ELEMENTS SUR LA FENETRE    '''
-        self.boutonStart.grid(row=1, column=0,padx=20,pady=5,sticky='N')
-        self.boutonPause.grid(row=1, column=1,padx=20,pady=5,sticky='N')
-        self.boutonStop.grid(row=1, column=2,padx=20,pady=5,sticky='N')
-        self.boutonManuel.grid(row=3,column=0, pady=5,sticky='N')
-        self.boutonAvancer.grid(row=3, column=1, pady=5, sticky='N')
-        self.boutonGauche.grid(row=3, column=2, pady=5, sticky='N')
-        self.boutonDroite.grid(row=3, column=3, pady=5, sticky='N')
+        self.passageModeAuto()
 
-        self.labelTempsRun.grid(row=1, column=3, padx=20, pady=5, sticky='N')
-        self.caseEtat.grid(row=0, column=4, rowspan=4, sticky='NEWS')
-        self.labelCommandeAutomatique.grid(row=0, column=0, columnspan=4, sticky='S')
-        self.labelCommandeManuelle.grid(row=2, column=0, columnspan=4, sticky='S')
+        self.canvas.grid(row=0, column=0, sticky='NEWS')
 
-        self.canvas.grid(row=0, sticky='NEWS')
-        self.barreControle.grid(row=1, sticky='NEWS')
 
-        '''      CONFIGURATION DES LIGNES & COLONES DE LA FENETRE DE BASE      '''
-        self.grid_rowconfigure(0, weight=9)
-        self.grid_rowconfigure(1, weight=1)
-        self.grid_columnconfigure(0, weight=1)
 
         '''      CONFIGURATION DES LIGNES & COLONES DE LA BARRE DE CONTROLE      '''
-        self.barreControle.grid_columnconfigure(4, weight=4)
-        self.barreControle.grid_columnconfigure(3, weight=1)
-        self.barreControle.grid_columnconfigure(2, weight=1)
-        self.barreControle.grid_columnconfigure(1, weight=1)
-        self.barreControle.grid_columnconfigure(0, weight=1)
-        self.barreControle.grid_rowconfigure(0, weight=1)
-        self.barreControle.grid_rowconfigure(1, weight=1)
-        self.barreControle.grid_rowconfigure(2, weight=1)
-        self.barreControle.grid_rowconfigure(3, weight=1)
+        self.passageConfigHorizontale()
 
         '''      CREATION & INITIALISATION DU MENU     '''
         self.menuBar = Menu(self)
@@ -101,12 +80,17 @@ class Application(tk.Tk):
 
     def initialisationMenu(self):
         menuFile = Menu(self.menuBar, tearoff=0)
-        menuFile.add_command(label="New", command=self.alert)
-        menuFile.add_command(label="Open", command=self.alert)
-        menuFile.add_command(label="Change Config", command=self.alert)
-        menuFile.add_command(label="Paramètres Robot", command=self.alert)
+        menuFile.add_command(label="Nouveau", command=self.alert)
+        menuFile.add_command(label="Connecter", command=self.alert)
+        menuFile.add_command(label="Changer Configuration", command=self.changementConfig)
         menuFile.add_separator()
         menuFile.add_command(label="Quitter", command=self.quit)
+
+        menuEdit = Menu(self.menuBar, tearoff=0)
+        menuEdit.add_command(label="Mode Automatique", command=self.passageModeAuto)
+        menuEdit.add_command(label="Mode Manuel", command=self.passageModeManuel)
+        menuEdit.add_separator()
+        menuEdit.add_command(label="Paramètres Robot", command=self.alert)
 
         menuScan = Menu(self.menuBar, tearoff=0)
         menuScan.add_command(label="Run", command=self.alert)
@@ -116,9 +100,113 @@ class Application(tk.Tk):
         menuScan.add_command(label="Export Room", command=self.alert)
 
         self.menuBar.add_cascade(label="  Window  ", menu=menuFile)
+        self.menuBar.add_cascade(label="  Edit  ", menu=menuEdit)
         self.menuBar.add_cascade(label="  Scan  ", menu=menuScan)
 
         self.config(menu=self.menuBar, bg='black')
+
+    def passageModeManuel(self):
+        self.boutonStart.grid_forget()
+        self.boutonPause.grid_forget()
+        self.boutonStop.grid_forget()
+        if self.affichageVertical:
+            self.boutonAvancer.grid(row=1,column=0, pady=5, sticky='N')
+            self.boutonGauche.grid(row=2,column=0, pady=5, sticky='N')
+            self.boutonDroite.grid(row=3,column=0, pady=5, sticky='N')
+        else:
+            self.boutonAvancer.grid(row=1, column=0, pady=5, sticky='N')
+            self.boutonGauche.grid(row=1, column=1, pady=5, sticky='N')
+            self.boutonDroite.grid(row=1, column=2, pady=5, sticky='N')
+        self.labelCommande["text"] = "Commande Manuelle"
+
+        self.modeAutomatique = False
+
+    def passageModeAuto(self):
+        self.boutonAvancer.grid_forget()
+        self.boutonGauche.grid_forget()
+        self.boutonDroite.grid_forget()
+        if self.affichageVertical:
+            self.boutonStart.grid(row=1,column=0, pady=5, sticky='N')
+            self.boutonPause.grid(row=2,column=0, pady=5, sticky='N')
+            self.boutonStop.grid(row=3,column=0, pady=5, sticky='N')
+        else:
+            self.boutonStart.grid(row=1, column=0,  pady=5, sticky='N')
+            self.boutonPause.grid(row=1, column=1,  pady=5, sticky='N')
+            self.boutonStop.grid(row=1, column=2,  pady=5, sticky='N')
+        self.labelCommande["text"] = "Commande Automatique"
+
+        self.modeAutomatique = True
+
+    def changementConfig(self):
+        self.caseEtat.grid_forget()
+        self.telecomande.grid_forget()
+        self.labelTempsRun.grid_forget()
+        self.labelCommande.grid_forget()
+        self.barreControle.grid_forget()
+        if self.affichageVertical:
+            self.passageConfigHorizontale()
+        else:
+            self.passageConfigVerticale()
+        if self.modeAutomatique:
+            self.passageModeAuto()
+        else:
+            self.passageModeManuel()
+
+    def passageConfigHorizontale(self):
+        """     PLACEMENT DES ELEMENTS SUR LA FENETRE    """
+        self.caseEtat.grid(row=0, column=1, rowspan=2, sticky='NEWS')
+        self.telecomande.grid(row=0, column=0, rowspan=2, sticky='NEWS')
+        self.labelTempsRun.grid(row=1, column=3, pady=5, sticky='N')
+        self.labelCommande.grid(row=0, column=0, columnspan=4)
+        self.barreControle.grid(row=1, sticky='NEWS')
+
+        self.barreControle.grid_columnconfigure(1, weight=1)
+        self.barreControle.grid_columnconfigure(0, weight=1)
+        self.barreControle.grid_rowconfigure(0, weight=1)
+        self.barreControle.grid_rowconfigure(1, weight=1)
+
+        self.telecomande.grid_columnconfigure(0, weight=4)
+        self.telecomande.grid_columnconfigure(1, weight=2)
+        self.telecomande.grid_columnconfigure(2, weight=2)
+        self.telecomande.grid_columnconfigure(3, weight=2)
+        self.telecomande.grid_rowconfigure(0, weight=1)
+        self.telecomande.grid_rowconfigure(1, weight=1)
+
+        '''      CONFIGURATION DES LIGNES & COLONES DE LA FENETRE DE BASE      '''
+        self.grid_rowconfigure(0, weight=10)
+        self.grid_rowconfigure(1, weight=1)
+        self.grid_columnconfigure(0, weight=3)
+        self.grid_columnconfigure(1, weight=0)
+
+        ''' CHANGEMENT VARIABLE '''
+        self.affichageVertical = False
+
+    def passageConfigVerticale(self):
+        """     PLACEMENT DES ELEMENTS SUR LA FENETRE    """
+        self.caseEtat.grid(row=1, column=0, sticky='NEWS')
+        self.telecomande.grid(row=0, column=0, sticky='NEWS')
+        self.labelTempsRun.grid(row=4, column=0, pady=5, sticky='N')
+        self.labelCommande.grid(row=0, column=0)
+        self.barreControle.grid(row=0, column=1, sticky='NEWS')
+
+        self.barreControle.grid_columnconfigure(0, weight=1)
+        self.barreControle.grid_columnconfigure(1, weight=0)
+        self.barreControle.grid_rowconfigure(0, weight=1)
+        self.barreControle.grid_rowconfigure(1, weight=1)
+
+        self.telecomande.grid_rowconfigure(0, weight=1)
+        self.telecomande.grid_rowconfigure(1, weight=1)
+        self.telecomande.grid_rowconfigure(2, weight=1)
+        self.telecomande.grid_rowconfigure(3, weight=1)
+        self.telecomande.grid_columnconfigure(0, weight=1)
+
+        '''      CONFIGURATION DES LIGNES & COLONES DE LA FENETRE DE BASE      '''
+        self.grid_rowconfigure(0, weight=10)
+        self.grid_rowconfigure(1, weight=0)
+        self.grid_columnconfigure(0, weight=10)
+        self.grid_columnconfigure(1, weight=1)
+
+        self.affichageVertical = True
 
     def startModeAutomatique(self):
         self.labelTempsRun["text"] = "Temps 10.00.00"
@@ -127,7 +215,7 @@ class Application(tk.Tk):
         self.boutonStart["state"] = DISABLED
         self.boutonStop["state"] = ACTIVE
         self.boutonPause["state"] = ACTIVE
-        self.boutonManuel["state"] = DISABLED
+        #self.boutonManuel["state"] = DISABLED
 
     def pauseModeAutomatique(self):
         self.labelTempsRun["text"] = "Temps 10.00.00"
@@ -144,12 +232,12 @@ class Application(tk.Tk):
         self.boutonStart["state"] = ACTIVE
         self.boutonStop["state"] = DISABLED
         self.boutonPause["state"] = DISABLED
-        self.boutonManuel["state"] = ACTIVE
+        #self.boutonManuel["state"] = ACTIVE
 
+'''
     def clilcModeManuel(self):
         if self.boutonManuel["background"] == "red":
-
-            '''    MISE A JOUR GRAPHIQUE   '''
+            
             self.boutonManuel["background"] = "green"
             self.boutonAvancer["state"] = ACTIVE
             self.boutonGauche["state"] = ACTIVE
@@ -160,17 +248,14 @@ class Application(tk.Tk):
 
         else:
 
-            '''    MISE A JOUR GRAPHIQUE   '''
             self.boutonManuel["background"] = "red"
             self.boutonAvancer["state"] = DISABLED
             self.boutonGauche["state"] = DISABLED
             self.boutonDroite["state"] = DISABLED
             self.boutonStart["state"] = ACTIVE
+'''
 
 if __name__ == "__main__":
     app = Application()
     app.title("Rammus Scanning")
-    imgStart = PhotoImage(file="../img/appContent/start.png").subsample(20, 20)
-    imgStop = PhotoImage(file="../img/appContent/stop.png").subsample(8, 8)
-    imgPause = PhotoImage(file="../img/appContent/pause.png").subsample(38, 38)
     app.mainloop()
